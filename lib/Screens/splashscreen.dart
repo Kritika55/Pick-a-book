2944dfe5eb5ac15f03eccdf8d93321d50fb5cf80
Kritika55 <94155352+Kritika55@users.noreply.github.com>
2023-01-12 20:10:32 +0545

    Splash Screen created

---
+++ lib/Screens/splashscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

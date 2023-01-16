6b946e0679bf03254626e93c7cd2ffe1c8a22785
Kritika55 <94155352+Kritika55@users.noreply.github.com>
2023-01-16 20:45:06 +0545

    Basic design done

--- lib/Screens/loginscreen.dart
+++ lib/Screens/loginscreen.dart
@@ -26,6 +26,61 @@ class _LoginScreenState extends State<LoginScreen> {
                 'Welcome \n back',
                 style: TextStyle(color: Colors.black, fontSize: 33),
               ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.5,
+                      right: 35,
+                      left: 35),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                            fillColor: Colors.grey,
+                            filled: true,
+                            hintText: 'Email',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            fillColor: Colors.grey,
+                            filled: true,
+                            hintText: 'Password',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Sign In',
+                            style: TextStyle(
+                                color: Color(0xff4c505b),
+                                fontSize: 27,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Color(0xff4c505b),
+                            child: IconButton(
+                              color: Colors.white,
+                              onPressed: () {},
+                              icon: Icon(Icons.arrow_forward),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )),
             )
           ],
         ),
